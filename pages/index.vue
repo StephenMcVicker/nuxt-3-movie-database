a2756c571b260d1f860d539ae333c803ebc6a998
StephenMcVicker <mcvickerstephen@gmail.com>
2023-04-01 20:12:42 +0100

    fix(index): correct API response

--- pages/index.vue
+++ pages/index.vue
@@ -5,18 +5,37 @@ section
   input(type="text"
         placeholder="Search for a movie"
         v-model="searchTerm")
-section.results
-</template>
+button(@click="search") Search
+section.results(v-if="results")
+  h2 Results
+    ul
+      li(v-for="result in results.results"
+          :key="result.id")
+        span {{ result.title }}
+  </template>
 
-<script setup lang="ts">
-import { APIResponse } from '@/types/APIResponse';
+  <script setup lang="ts">
 
-const searchTerm = ref('');
+  import type {APIResponse} from "@/types/APIResponse";
 
-const url = computed (() => {
-  return `api/movies/search?query=${searchTerm.value}`;
-})
+  const searchTerm = ref('');
+  const results = ref<APIResponse>();
 
-const { data } = useFetch<APIResponse>(url);
+  const url = computed (() => {
+    return `api/movies/search?query=${searchTerm.value}`;
+  })
 
-</script>
+  const search = () => {
+    console.log(searchTerm.value);
+    const {data, error} = useFetch<APIResponse>(url.value);
+
+    if (error.value) {
+      console.log(error.value);
+    }
+
+    if (data.value) {
+      results.value = data.value;
+      console.log(results.value);
+    }
+  }
+  </script>
